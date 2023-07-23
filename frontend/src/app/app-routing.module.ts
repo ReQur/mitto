ce7bcfc017109771bca7b946b904d80ae4e986f0
ReQur <dank2801@gmail.com>
2023-07-23 12:56:55 +0500

    Revoce unused routings

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { UserInfoComponent } from './components/user-info/user-info.component';
-import { RefreshTokenComponent } from './components/refresh-token/refresh-token.component';
 import { MainPageComponent } from "./pages/main-page/main-page.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirect to `login` by default
-  { path: 'login', component: LoginComponent },
-  { path: 'me', component: UserInfoComponent },
-  { path: 'refresh', component: RefreshTokenComponent },
+  { path: '', redirectTo: '/main-page', pathMatch: 'full' }, // Redirect to `main-page` by default
   { path: 'main-page', component: MainPageComponent },
 ];
 
