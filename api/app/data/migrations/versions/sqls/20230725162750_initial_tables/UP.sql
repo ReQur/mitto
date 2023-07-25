a315adf32ff201a6f5600db5c86c2de74b5a4292
ReQur <dank2801@gmail.com>
2023-07-25 16:44:46 +0500

    Add migration with initial tables

---
+++ api/app/data/migrations/versions/sqls/20230725162750_initial_tables/UP.sql
@@ -0,0 +1,24 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    username VARCHAR(255),
+    password VARCHAR(255) NOT NULL,
+    is_active BOOLEAN NOT NULL DEFAULT true
+);
+
+CREATE TABLE chat (
+    id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE user_chat (
+    user_id INTEGER REFERENCES users(id),
+    chat_id INTEGER REFERENCES chat(id),
+    PRIMARY KEY(user_id, chat_id)
+);
+
+CREATE TABLE message (
+    id SERIAL PRIMARY KEY,
+    text VARCHAR(4000) NOT NULL,
+    owner_id INTEGER REFERENCES users(id),
+    chat_id INTEGER REFERENCES chat(id)
+);
